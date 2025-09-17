2ffef1053cc6de53732b27e7175c3586919217e0
Viivi1 <bhq640@myy.haaga-helia.fi>
2025-09-17 13:44:13 +0300

    tehtC4.1

--- src/main/java/hh/backend/bookstore/BookstoreApplication.java
+++ src/main/java/hh/backend/bookstore/BookstoreApplication.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 
 import hh.backend.bookstore.domain.Book;
 import hh.backend.bookstore.domain.BookRepository;
+import hh.backend.bookstore.domain.Category;
+import hh.backend.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -20,12 +22,22 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 	@Bean
-public CommandLineRunner demo(BookRepository repository) {
+	public CommandLineRunner demo(BookRepository repository) {
 	return (args) -> {
 	  	log.info("save a couple of books");
 		repository.save(new Book("The Hobbit", "J.R.R. Tolkien", 1937, "123456789", 19.99));
 		repository.save(new Book("1984", "George Orwell", 1949, "987654321", 14.99));
 		repository.save(new Book("Kafka on the Shore", "Haruki Murakami", 2002, "112345111", 21.99));
+		
+	};
+}
+	@Bean
+	public CommandLineRunner categoryDemo(CategoryRepository categoryRepository) {
+	return (args) -> {
+		log.info("save some categories");
+		categoryRepository.save(new Category(null, "Fantasy"));
+		categoryRepository.save(new Category(null, "Scifi"));
+		categoryRepository.save(new Category(null, "History"));		
 	};
 	}
 }
